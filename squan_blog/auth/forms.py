cf68828e130b99786185e551da340c4e5c45d6fe
HYSkyline <HYSkyline@163.com>
2017-07-05 00:30:19 +0800

    实现登录状态，实现注册

--- squan_blog/auth/forms.py
+++ squan_blog/auth/forms.py
@@ -1,13 +1,25 @@
 # -*- coding:utf-8 -*-
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
-class LoginForm(Form):
-	username = StringField('Username', validators=[Required()])
-	password = PasswordField('Password', validators=[Required()])
-	remember_me = BooleanField('Remember me')
-	submit = SubmitField('Submit')
-	
+class LoginForm(FlaskForm):
+	username = StringField(u'用户名', validators=[Required()])
+	password = PasswordField(u'密码', validators=[Required()])
+	remember_me = BooleanField(u'保持登录状态')
+	submit = SubmitField('登录')
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField(u'用户名', validators=[Required(), Length(1, 32)])
+	password = PasswordField(u'密码', validators=[Required(), EqualTo('password2', message=u'两次密码需要相同')])
+	password2 = PasswordField(u'确认密码', validators=[Required()])
+	submit = SubmitField(u'注册')
+
+	def validate_username(self, field):
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError(u'用户名已被注册')
--- squan_blog/auth/view.py
+++ squan_blog/auth/view.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, login_required
+from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -23,6 +24,20 @@ def logout():
 	logout_user()
 	flash('You have logged out.')
 	return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(
+			username=form.username.data,
+			password=form.password.data
+		)
+		db.session.add(user)
+		flash('You can login now.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
 
 
 @auth.route('/secret')
