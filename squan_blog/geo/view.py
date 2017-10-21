5cde913f24d09baed2a557385913f2b11b79c26c
HYSkyline <HYSkyline@163.com>
2017-10-22 01:21:57 +0800

    geo core finished. geo css going to work. v17102201.

--- squan_blog/geo/view.py
+++ squan_blog/geo/view.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from flask import render_template, session, redirect, url_for, current_app, request
+from flask import render_template, session, redirect, url_for, current_app, request, jsonify
 from flask_login import login_required, current_user
 from .. import db, geo_engine
 from ..models import Geopoint, Geopolyline, Geopolygon, GeoBase
@@ -12,6 +12,7 @@ from shapely.geometry import Point, LineString, Polygon
 import json
 import time
 import copy
+import ast
 
 
 @geo.route('/view', methods=['GET', 'POST'])
@@ -134,82 +135,86 @@ def geo_edit(project_name):
 			'polyline': pl_list,
 			'polygon': pg_list
 		}
-		return render_template('geo/geoedit.html', geom=geom)
+		return render_template('geo/geoedit.html', geom=geom, projectname=project_name)
 	if request.method == 'POST':
-		geo_id = long(float(request.form.get('geoId')))
-		geo_projectname = project_name
-		geo_quizee = current_user.username
-		geo_type = request.form.get('geoType')
-		geo_coord = request.form.get('geoCoordinate')
-		geo_event = request.form.get('geoEvent')
-		print ''
-		print ''
-		print geo_type.encode('utf-8') + ' ' + str(geo_id) + '  processing.'
-		print ''
-		print ''
-		if geo_event == 'DELETED':
-			if geo_type == 'POINT':
-				pt_res = geo_session.query(Geopoint).filter_by(ptid=geo_id).first()
-				geo_session.delete(pt_res)
-			elif geo_type == 'LINESTRING':
-				pl_res = geo_session.query(Geopolyline).filter_by(plid=geo_id).first()
-				geo_session.delete(pl_res)
-			elif geo_type == 'POLYGON':
-				pg_res = geo_session.query(Geopolygon).filter_by(pgid=geo_id).first()
-				geo_session.delete(pg_res)
-		else:
-			if geo_type == 'POINT':
-				pt_res = geo_session.query(Geopoint).filter_by(ptid=geo_id).first()
-				if pt_res:
-					pt_lng, pt_lat = geo_coord.encode('utf-8').split(' ')
-					pt_res.geopt = from_shape(Point(float(pt_lng), float(pt_lat)), srid=4326)
-				else:
-					geo_session.add(
-						Geopoint(
-							ptid=geo_id,
-							projectname=geo_projectname,
-							quizee=geo_quizee,
-							quiztime=time.time(),
-							geopt=geo_type + '(' + geo_coord.encode('utf-8') + ')'
-						)
-					)
-			elif geo_type == 'LINESTRING':
-				pl_res = geo_session.query(Geopolyline).filter_by(plid=geo_id).first()
-				if pl_res:
-					polyline_point_list = []
-					for each_point in geo_coord.encode('utf-8').split(','):
-						pt_lng, pt_lat = each_point.split(' ')
-						polyline_point_list.append((float(pt_lng), float(pt_lat)))
-					pl_res.geopl = from_shape(LineString(polyline_point_list), srid=4326)
-				else:
-					geo_session.add(
-						Geopolyline(
-							plid=geo_id,
-							projectname=geo_projectname,
-							quizee=geo_quizee,
-							quiztime=time.time(),
-							geopl=geo_type + '(' + geo_coord.encode('utf-8') + ')'
-						)
-					)
-			elif geo_type == 'POLYGON':
-				pg_res = geo_session.query(Geopolygon).filter_by(pgid=geo_id).first()
-				if pg_res:
-					polygon_point_list = []
-					for each_point in geo_coord.encode('utf-8').split(',')[:-1]:
-						pt_lng, pt_lat = each_point.split(' ')
-						polygon_point_list.append((float(pt_lng), float(pt_lat)))
-					pg_res.geopg = from_shape(Polygon(polygon_point_list), srid=4326)
-				else:
-					geo_session.add(
-						Geopolygon(
-							pgid=geo_id,
-							projectname=geo_projectname,
-							quizee=geo_quizee,
-							quiztime=time.time(),
-							geopg='POLYGON' + '((' + geo_coord.encode('utf-8') + '))'
-						)
-					)
+		try:
+			geo_id = long(float(request.form.get('geo_id')))
+			geo_projectname = project_name
+			geo_quizee = current_user.username
+			geo_type = request.form.get('geo_type')
+			geo_coord = request.form.get('geo_coordinate')
+			geo_event = request.form.get('geo_event')
+			if geo_event == 'DELETED':
+				if geo_type == 'POINT':
+					pt_res = geo_session.query(Geopoint).filter_by(ptid=geo_id).first()
+					geo_session.delete(pt_res)
+				elif geo_type == 'LINESTRING':
+					pl_res = geo_session.query(Geopolyline).filter_by(plid=geo_id).first()
+					geo_session.delete(pl_res)
+				elif geo_type == 'POLYGON':
+					pg_res = geo_session.query(Geopolygon).filter_by(pgid=geo_id).first()
+					geo_session.delete(pg_res)
 			else:
-				pass
-		geo_session.commit()
-	return ('', 204)
+				if geo_type == 'POINT':
+					pt_res = geo_session.query(Geopoint).filter_by(ptid=geo_id).first()
+					pt_list = ast.literal_eval(geo_coord)
+					if pt_res:
+						pt_res.geopt = from_shape(Point(float(pt_list[0]), float(pt_list[1])), srid=4326)
+					else:
+						geo_session.add(
+							Geopoint(
+								ptid=geo_id,
+								projectname=geo_projectname,
+								quizee=geo_quizee,
+								quiztime=time.time(),
+								geopt=geo_type + '(' + str(pt_list[0]) + ' ' + str(pt_list[1]) + ')'
+							)
+						)
+				elif geo_type == 'LINESTRING':
+					pl_res = geo_session.query(Geopolyline).filter_by(plid=geo_id).first()
+					pl_list = ast.literal_eval(geo_coord)
+					if pl_res:
+						polyline_point_list = []
+						for each_point in pl_list:
+							polyline_point_list.append((float(each_point[0]), float(each_point[1])))
+						pl_res.geopl = from_shape(LineString(polyline_point_list), srid=4326)
+					else:
+						polyline_point_list = []
+						for each_point in pl_list:
+							polyline_point_list.append(str(each_point[0]) + ' ' + str(each_point[1]))
+						geo_session.add(
+							Geopolyline(
+								plid=geo_id,
+								projectname=geo_projectname,
+								quizee=geo_quizee,
+								quiztime=time.time(),
+								geopl=geo_type + '(' + ','.join(polyline_point_list) + ')'
+							)
+						)
+				elif geo_type == 'POLYGON':
+					pg_res = geo_session.query(Geopolygon).filter_by(pgid=geo_id).first()
+					pg_list = ast.literal_eval(geo_coord)
+					if pg_res:
+						polygon_point_list = []
+						for each_point in pg_list:
+							polygon_point_list.append((float(each_point[0]), float(each_point[1])))
+						pg_res.geopg = from_shape(Polygon(polygon_point_list), srid=4326)
+					else:
+						polygon_point_list = []
+						for each_point in pg_list:
+							polygon_point_list.append(str(each_point[0]) + ' ' + str(each_point[1]))
+						geo_session.add(
+							Geopolygon(
+								pgid=geo_id,
+								projectname=geo_projectname,
+								quizee=geo_quizee,
+								quiztime=time.time(),
+								geopg=geo_type + '((' + ','.join(polygon_point_list) + '))'
+							)
+						)
+				else:
+					pass
+			geo_session.commit()
+			return jsonify({'status': 200, 'info': geo_type + ' ' + str(geo_id) + ' executed.'})
+		except Exception as e:
+			return jsonify({'status': 500, 'info': e.message})
