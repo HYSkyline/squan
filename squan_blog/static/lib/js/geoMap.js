59caed92da58cc44e583150f54033de00b326b1f
HYSkyline <HYSkyline@163.com>
2017-10-22 11:44:53 +0800

    geoMap.js added. v17102211.

---
+++ squan_blog/static/lib/js/geoMap.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([30.24105142736931, 120.14077259673466], 12);
+var streetMap = L.layerGroup([L.tileLayer('http://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})]);
+var googleMap = L.layerGroup([L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&x={x}&y={y}&z={z}', {
+    attribution: 'Map data &copy; 2017 <a href="https://www.google.com/permissions/geoguidelines.html">Google</a>'
+})]);
+var esriMap = L.layerGroup([L.esri.basemapLayer('Imagery')]);
+var mapBase = {
+    '街道地图': streetMap,
+    '卫星影像': googleMap,
+    'ESRI HD': esriMap
+};
+map.addLayer(streetMap);
+drawnItems = L.featureGroup();
+L.control.layers(
+	{
+        '矢量地图': streetMap,
+        '卫星影像': googleMap,
+        'ESRI HD': esriMap
+    },
+    {
+    	'绘制图层': drawnItems
+    },
+    {
+    	position: 'topright',
+    	collapsed: true
+    }
+).addTo(map);
